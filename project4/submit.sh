9a9c5114e26142d0ee74eabeb062ae86d354dca4
chorse <chorse@ubuntu.(none)>
2019-05-24 21:49:34 -0700

    project 4 complete

---
+++ project4/submit.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm -rf 20180336_assign4
+rm -f 20180336_assign4.tar.gz
+mkdir -p 20180336_assign4
+cp mydc.s readme EthicsOath.pdf 20180336_assign4
+tar zcf 20180336_assign4.tar.gz 20180336_assign4
---
+++ project4/test.c
@@ -0,0 +1,6 @@
+int main(){
+	int a,b;
+	a = -0x1234;
+	b = 0x12;
+	return a%b;
+}
